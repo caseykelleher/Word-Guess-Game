6012d2dd4af6ca65715ffb9d5a8a475dcabd8706
Casey K <caseykelleher@icloud.com>
2019-10-20 18:15:37 -0400

    added reset.css

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+<script>
+        //my array//
+        var movieTitle = {
+           "Little Mermaid",
+           "Toy Story",
+           "The Lion King",
+           "Beauty and the Beast",
+           "Seven Dwarfs"
+        };
+
+        var movieTitle = movieTitle[Math.floor(Math.random() * movieTitle.length)];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </script>
+       
